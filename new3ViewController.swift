3777c4dce6fba3ef1d0e0757a6a91f1cc697a6d7
ivanasmiljic <iwana_iv@yahoo.com>
2017-10-22 00:03:20 +0200

    new 3 commit

---
+++ new3ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  new3ViewController.swift
+//  Segmentio
+//
+//  Created by NS WD on 10/22/17.
+//  Copyright © 2017 Yalantis. All rights reserved.
+//
+
+import UIKit
+
+class new3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
